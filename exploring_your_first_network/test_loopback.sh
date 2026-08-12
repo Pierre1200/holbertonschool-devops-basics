01ed1301e7abe03a63cccb98edc9f07081061d1f
Pierre1200 <rouvellat.p@icloud.com>
2026-08-12 11:29:34 +0200

    Fix quoting in loopback test script to ensure proper execution

--- exploring_your_first_network/test_loopback.sh
+++ exploring_your_first_network/test_loopback.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ping -c 4 $(ip -4 -br addr show scope host | tr -s ' ' | cut -d ' ' -f 3 | head -n 1 | cut -d '/' -f 1)
+ping -c 4 "$(ip -4 -br addr show scope host | tr -s ' ' | cut -d ' ' -f 3 | head -n 1 | cut -d '/' -f 1)"
