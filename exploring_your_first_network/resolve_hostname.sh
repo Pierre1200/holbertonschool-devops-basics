b28bdf6698934574f334f767e542dfe4e7c55818
Pierre1200 <rouvellat.p@icloud.com>
2026-08-13 10:35:51 +0200

    Replace hostname resolution logic with getent command for improved functionality

--- exploring_your_first_network/resolve_hostname.sh
+++ exploring_your_first_network/resolve_hostname.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-# This script will resolve the hostname of the system
-# It will use the hostname command to get the hostname and then resolve it to an IP address
-hostname -I | awk '{print $1}'
+getent hosts "$1"
